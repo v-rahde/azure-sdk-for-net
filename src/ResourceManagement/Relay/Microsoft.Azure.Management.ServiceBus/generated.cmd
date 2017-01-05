603438ac7edab9657a50f1bdb06e6e1d2d6ff5bc
v-rahde <v-rahde@microsoft.com>
2017-01-05 14:06:38 +0530

    Relay SDK

---
+++ src/ResourceManagement/Relay/Microsoft.Azure.Management.ServiceBus/generated.cmd
@@ -0,0 +1,18 @@
+::
+:: Microsoft Azure SDK for Net - Generate library code
+:: Copyright (C) Microsoft Corporation. All Rights Reserved.
+::
+
+@echo off
+set autoRestVersion=0.16.0-Nightly20160426 
+if  "%1" == "" (
+    set specFile="C:\Users\v-rahde\Documents\My Received Files\relay.json"
+		
+) else (
+    set specFile="%1"
+)
+set repoRoot=%~dp0..\..\..\..
+set generateFolder=%~dp0GeneratedRelay
+
+if exist %generateFolder% rd /S /Q  %generateFolder%
+call "%repoRoot%\tools\autorest.gen.cmd" %specFile% Microsoft.Azure.Management.Relay %autoRestVersion% %generateFolder% 
